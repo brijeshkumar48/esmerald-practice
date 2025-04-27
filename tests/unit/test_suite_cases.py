2575b9c2d7cdfe179fe59c6e880c8b72dc51ec3f
unknown <brijeshkumar48@yahoo.com>
2025-04-27 13:48:50 +0530

    git ignore updated

--- tests/unit/test_suite_cases.py
+++ tests/unit/test_suite_cases.py
@@ -6,10 +6,7 @@ from main import app
 from esmerald.testclient import EsmeraldTestClient
 
 
-class TestMathOperations:
-    """
-    Test basic math operations.
-    """
+class TestllmModel:
 
     def teardown_method(self):
         """
@@ -37,25 +34,3 @@ class TestMathOperations:
 
         assert response.status_code == 201
         assert response.json()["data"]["response"] == fake_llm_response
-
-    @pytest.mark.asyncio
-    async def test_addition(self):
-        """
-        Test asynchronous addition logic.
-        """
-        result = await self.async_add(2, 3)
-        assert result == 5
-
-    @pytest.mark.asyncio
-    async def test_multiplication(self):
-        """
-        Test asynchronous multiplication logic.
-        """
-        result = await self.async_multiply(4, 5)
-        assert result == 20
-
-    async def async_add(self, a, b):
-        return a + b
-
-    async def async_multiply(self, a, b):
-        return a * b
